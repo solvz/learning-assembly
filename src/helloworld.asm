d2522bd3836a62a9593f53d2195597c1979979f0
solvz <krishnarajeev2005@gmail.com>
2025-02-26 02:35:07 +0530

    Hello World

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -1,3 +1,18 @@
+global _start
+
 section .text   ; declaring .text section
 
+_start:
+    mov eax, 0x4 ; write syscall
+    mov ebx, 1   ; use stdout as the file descriptor
+    mov ecx, message ; mess is buffer
+    mov edx, message_length ; supplying length
+    int 0x80     ; invoke syscall
+
+    mov eax, 0x1 ; exit syscall
+    mov ebx, 0   ; return
+    int 0x80
+
 section .data   ; declaring data section
+    message: db "Hello World!", 0xA
+    message_length equ $-message
